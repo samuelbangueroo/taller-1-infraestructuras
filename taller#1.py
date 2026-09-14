eaad206f5a2b910e1545bd7d671d09cc5fd9b6bf
guxky <samuelbangueroortega@gmail.com>
2026-09-13 20:51:39 -0500

    calculo fibonacci, tiempos y paralelizacion de procesos

--- taller#1.py
+++ taller#1.py
@@ -10,22 +10,41 @@ Samuel Banguero Ortega - 2418671
 import time
 import concurrent.futures
 
-N = 20  # Número de Fibonacci a calcular
+N = 6666 # Número de Fibonacci a calcular
 
 def fibonacci(n):
     """
-    Calcula el n-ésimo número de Fibonacci de forma recursiva y secuencial.
+    Calcula el n-ésimo número de Fibonacci.
     """
     
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
+def calcular_fibonacci_secuencial(n_elementos):
+    """
+    Calcula los números de Fibonacci de forma secuencial.
+    Se utiliza como referencia para comparar el rendimiento.
+    """
+
+    inicio = time.time()
+
+    resultados = [] * n_elementos
+
+    for i in range(n_elementos):
+        resultados.append(fibonacci(i))
+
+    fin = time.time()
+    tiempo = fin - inicio
+
+    print(f"Fibonacci secuencial ({n_elementos}): {resultados}")
+    return tiempo
 
 def calcular_fibonacci_paralelo(n_elementos, executor_type):
     """
     Calcula los números de Fibonacci utilizando
-    un Executor de concurrent.futures.
+    un Executor de concurrent.futures
     """
 
     inicio = time.time()
@@ -59,12 +78,17 @@ def calcular_fibonacci_paralelo(n_elementos, executor_type):
     Siempre serial, se hace una sola despues de que todos los futures hayan terminado
     evitando conflictos de acceso 
     """
-    print(f"Fibonacci ({n_elementos}): {resultados}")
-    print(f"Tiempo de ejecución: {tiempo_ejecucion:.4f} segundos")
+    print(f"Fibonacci paralelo ({n_elementos}): {resultados}")
+    return tiempo_ejecucion
+
+def tiempos(t_paralelo, t_secuencial):
+    print(f"Tiempo de ejecución paralelo: {t_paralelo:.4f} segundos")
+    print(f"Tiempo de ejecución secuencial: {t_secuencial:.4f} segundos")
 
 
 if __name__ == "__main__":
-    calcular_fibonacci_paralelo(
-        N,
-        concurrent.futures.ThreadPoolExecutor
-    )
+    
+    tiempos(
+        calcular_fibonacci_paralelo(N,concurrent.futures.ProcessPoolExecutor),
+        calcular_fibonacci_secuencial(N)
+        )
